a933e0b936f71c350be2034131b56dd50de6cdcb
Tyler Long <tyler4long@gmail.com>
2017-01-01 18:02:42 +0800

    Finish the skeleton

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const iconsPlugin = (md, options) => {
-  // const arrayReplaceAt = md.utils.arrayReplaceAt
+  const arrayReplaceAt = md.utils.arrayReplaceAt
 
   md.renderer.rules.emoji = (tokens, idx) => {
     return tokens[idx].content
   }
 
   md.core.ruler.push('icons', (state) => {
+    let level = 0
     for (let i = 0; i < state.tokens.length; i++) {
       if (state.tokens[i].type !== 'inline') {
         continue
@@ -13,14 +14,11 @@ const iconsPlugin = (md, options) => {
       let tokens = state.tokens[i].children
       for (let j = tokens.length - 1; j >= 0; j--) {
         let token = tokens[j]
-        if (token.type === 'link_open' || token.type === 'link_close') {
-          console.log(`token.type: ${token.type} | token.info: ${token.info} | token.nesting: ${token.nesting}`)
-          if (token.info === 'auto') {
-            console.log(`token.nesting: ${token.nesting}`)
-          }
+        if ((token.type === 'link_open' || token.type === 'link_close') && token.info === 'auto') {
+          level -= token.nesting
         }
-        if (token.type === 'text' && options.regex.test(token.content)) {
-          // state.tokens[i].children = tokens = arrayReplaceAt(tokens, j, )
+        if (level === 0 && token.type === 'text' && options.regex.test(token.content)) {
+          state.tokens[i].children = tokens = arrayReplaceAt(tokens, j, [tokens[j]])
         }
       }
     }
