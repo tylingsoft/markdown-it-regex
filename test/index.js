007556ca80e74d6c0ff65bcb3f6be04b7ed1be65
Tyler Long <tyler4long@gmail.com>
2017-01-01 19:11:57 +0800

    Update test

--- test/index.js
+++ test/index.js
@@ -2,9 +2,7 @@ import assert from 'assert'
 import markdownIt from 'markdown-it'
 import markdownItIcons from '../src/index'
 
-let mdi = markdownIt({
-  linkify: true
-})
+let mdi = markdownIt()
 mdi = mdi.use(markdownItIcons, {
   regex: /(:heart:)/,
   replace: (match) => {
@@ -14,10 +12,9 @@ mdi = mdi.use(markdownItIcons, {
 
 assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>')
 assert(mdi.render('Hello world').trim() === '<p>Hello world</p>')
-assert(mdi.render('http://baidu.com').trim() === '<p><a href="http://baidu.com">http://baidu.com</a></p>')
-assert(mdi.render('baidu.com').trim() === '<p><a href="http://baidu.com">baidu.com</a></p>')
 
 assert(mdi.render(':heart:').trim() === '<p><i class="e1a-heart"></i></p>')
 assert(mdi.render('I :heart: you').trim() === '<p>I <i class="e1a-heart"></i> you</p>')
+assert(mdi.render('I :heart: :heart: :heart: you').trim() === '<p>I <i class="e1a-heart"></i> <i class="e1a-heart"></i> <i class="e1a-heart"></i> you</p>')
 
 console.log(mdi.render('I :heart: :heart: :heart: you'))
