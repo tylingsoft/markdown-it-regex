5c3d3ceb68d90d89636a26fc04a55cc9807a47c9
Tyler Long <tyler4long@gmail.com>
2017-01-02 09:42:43 +0800

    Add more test cases

--- test/index.js
+++ test/index.js
@@ -10,6 +10,13 @@ mdi = mdi.use(markdownItIcons, {
     return `<i class="e1a-${match.slice(1, -1)}"></i>`
   }
 })
+mdi = mdi.use(markdownItIcons, {
+  name: 'font-awesome',
+  regex: /(:fa-(?:truck|rocket|leaf):)/,
+  replace: (match) => {
+    return `<i class="fa ${match.slice(1, -1)}"></i>`
+  }
+})
 
 assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>')
 assert(mdi.render('Hello world').trim() === '<p>Hello world</p>')
@@ -18,4 +25,7 @@ assert(mdi.render(':heart:').trim() === '<p><i class="e1a-heart"></i></p>')
 assert(mdi.render('I :heart: you').trim() === '<p>I <i class="e1a-heart"></i> you</p>')
 assert(mdi.render('I :heart: :heart: :heart: you').trim() === '<p>I <i class="e1a-heart"></i> <i class="e1a-heart"></i> <i class="e1a-heart"></i> you</p>')
 
+assert(mdi.render('A :fa-truck: runs').trim() === '<p>A <i class="fa fa-truck"></i> runs</p>')
+
 console.log(mdi.render('I :heart: :heart: :heart: you'))
+console.log(mdi.render('A :fa-truck: runs'))
